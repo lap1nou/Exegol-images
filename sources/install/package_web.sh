c0631628d1c113a73d4549c8f096b673030fd337
had <85348372+hadhub@users.noreply.github.com>
2025-07-23 21:23:28 +0200

    Fix installation of caido

--- sources/install/package_web.sh
+++ sources/install/package_web.sh
@@ -904,16 +904,17 @@ function install_zap() {
 }
 
 function install_caido() {
+    # CODE-CHECK-WHITELIST=add-aliases
+    colorecho "Installing Caido"
     mkdir /opt/tools/caido
     # Get the link of the last version in deb format
-    caido_latest_download_deb=$(curl -s https://api.caido.io/releases/latest | jq -r --arg arch "linux-$(uname -m).deb" '.links[].link | select(endswith($arch))')
+    local caido_latest_download_deb=$(curl -s https://api.caido.io/releases/latest | jq -r --arg arch "linux-$(uname -m).deb" '.links[].link | select(endswith($arch))')
     # Get the file name
-    caido_file_name=$(basename $caido_latest_download_deb)
+    local caido_file_name=$(basename $caido_latest_download_deb)
     # Download the deb file and store it into /opt/tools/caido
     wget $caido_latest_download_deb -O /opt/tools/caido/$caido_file_name
     # Install
-    dpkg -i /opt/tools/caido/$caido_file_name
-    # rm /opt/tools/caido/$caido_file_name for free up disk space?
+    dpkg -i "/opt/tools/caido/$caido_file_name"
     add-history caido
     add-test-command "which caido"
     add-to-list "caido,https://docs.caido.io/quickstart/,A lightweight web security auditing toolkit."
