e1c186fc4e72fca00d40916acc9176062037ebb0
lap1nou <lapinousexy@gmail.com>
2025-08-01 02:05:03 +0200

    fix: fixed bh-ce install

--- sources/install/package_ad.sh
+++ sources/install/package_ad.sh
@@ -193,7 +193,27 @@ function install_bloodhound-ce() {
     latestRelease=$(jq --raw-output 'first(.[] | select(.tag_name | contains("-rc") | not) | .tag_name)' "${curl_tempfile}")
     git -C "${bloodhoundce_path}" clone --depth 1 --branch "${latestRelease}" "https://github.com/SpecterOps/BloodHound.git" src
     cd "${bloodhoundce_path}/src/" || exit
-    catch_and_retry /bin/sh -c 'VERSION=v999.999.999 CHECKOUT_HASH="" python3 ./packages/python/beagle/main.py build --verbose --ci'
+
+    # Reference: https://github.com/SpecterOps/BloodHound/blob/main/dockerfiles/bloodhound.Dockerfile
+    # Build the UI
+    yarn install
+    yarn build
+    mkdir -p ./cmd/api/src/api/static/assets
+    cp -r ./cmd/ui/dist/. ./cmd/api/src/api/static/assets
+
+    # Build the API
+    git --no-pager -c 'versionsort.suffix=-rc' tag --list v*.*.* --sort=-v:refname | head -n 1 | sed 's/^v//' | awk \
+    -F'[.+-]' \
+    -v pkg="github.com/specterops/bloodhound/cmd/api/src/version" \
+    '{ major = $1; minor = $2; patch = $3; pre = ""; if ($4) pre = $4; \
+    printf("-X '\''%s.majorVersion=%s'\'' ", pkg, major); \
+    printf("-X '\''%s.minorVersion=%s'\'' ", pkg, minor); \
+    printf("-X '\''%s.patchVersion=%s'\''", pkg, patch); \
+    if (pre != "") \
+      printf(" -X '\''%s.prereleaseVersion=%s'\''", pkg, pre); \
+    }' > LDFLAGS
+    go build -C cmd/api/src -o ${bloodhoundce_path}/bloodhound -ldflags "$(cat LDFLAGS)" github.com/specterops/bloodhound/cmd/api/src/cmd/bhapi
+
     # Force remove go and yarn cache that are not stored in standard locations
     rm -rf "${bloodhoundce_path}/src/cache" "${bloodhoundce_path}/src/.yarn/cache"
 
