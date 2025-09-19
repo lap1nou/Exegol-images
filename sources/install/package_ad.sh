c7ded7d8657e398ba408859fb3b9af5e28b96cbd
QU35T-code <github@qu35t-mail.simplelogin.com>
2025-09-19 10:05:07 +0200

    Fix ExtractBitlockerKeys.py test command

--- sources/install/package_ad.sh
+++ sources/install/package_ad.sh
@@ -1289,7 +1289,7 @@ function install_extractbitlockerkeys() {
     deactivate
     add-aliases extractbitlockerkeys
     add-history extractbitlockerkeys
-    add-test-command "ExtractBitlockerKeys.py|& grep 'usage: ExtractBitlockerKeys.py'"
+    add-test-command "ExtractBitlockerKeys.py -h"
     add-to-list "ExtractBitlockerKeys,https://github.com/p0dalirius/ExtractBitlockerKeys,A system administration or post-exploitation script to automatically extract the bitlocker recovery keys from a domain."
 }
 
